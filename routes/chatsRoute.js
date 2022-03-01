48f35f94732c3f5d67f6b66ef539c8ed2d229d4b
Joseph Vega <josephvega@Josephs-Mini.lan>
2022-02-28 20:02:34 -0800

    renamed Routes directory

---
+++ routes/chatsRoute.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const RoomModel = require("../models/room_model");
+const MessageModel = require("../models/message_model");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+router.get("/rooms", async (req, res) => {
+  try {
+    const rooms = await RoomModel.find();
+    res.json(rooms);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.post("/rooms", upload.single("avatar"), async (req, res) => {
+  try {
+    const { roomName } = req.body;
+    const postRoom = await RoomModel.insert({
+      roomName: roomName,
+      roomImage: req.file.path,
+    });
+    res.json(postRoom[0]);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+router.get("/messages", async (req, res) => {
+  try {
+    const getMessages = await MessageModel.find();
+    console.log(getMessages);
+    res.json(getMessages);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.get("/messages/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getChat = await MessageModel.findById(id);
+    res.json(getChat);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.post("/messages", async (req, res) => {
+  try {
+    const postMessage = await MessageModel.insert(req.body);
+    res.json(postMessage[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.put("/latest-message", async (req, res) => {
+  try {
+    const { roomId, message } = req.body;
+    const updateLatestMessage = await RoomModel.update(message, roomId);
+    res.json(updateLatestMessage[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+module.exports = router;
---
+++ routes/userRoute.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const UserModel = require("../models/user_model");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getUserById = await UserModel.findById(id);
+    res.json(getUserById[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.put("/:id", upload.single("userAvatar"), async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateUser = await UserModel.update(id, req.file.path);
+    res.json(updateUser[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+module.exports = router;
